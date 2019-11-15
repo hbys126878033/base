7c541e998bffae1ad24709ff52f4d515b1e7117f
hbys126878033 <863533952@qq.com>
2019-11-15 11:10:53 +0800

    squash! return

---
+++ base-0106-dynamic-data-source/src/main/java/com/wondersgroup/base0106dynamicdatasource/Base0106DynamicDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.wondersgroup.base0106dynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base0106DynamicDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Base0106DynamicDataSourceApplication.class, args);
+    }
+
+}
---
+++ base-0106-dynamic-data-source/src/test/java/com/wondersgroup/base0106dynamicdatasource/Base0106DynamicDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wondersgroup.base0106dynamicdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base0106DynamicDataSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
